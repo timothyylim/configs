c78294231a6fa94b5a687abf32a7bfdf1fb80d38
timothyylim <timothylim23@gmail.com>
2026-02-23 15:41:18 +0800

    Remove Tokyo from world clock

--- .tmux/scripts/world-time.sh
+++ .tmux/scripts/world-time.sh
@@ -5,7 +5,6 @@ atlanta_hour=$(TZ=America/New_York date '+%H' | sed 's/^0//')
 oslo_hour=$(TZ=Europe/Oslo date '+%H' | sed 's/^0//')
 jakarta_hour=$(TZ=Asia/Jakarta date '+%H' | sed 's/^0//')
 kuala_lumpur_hour=$(TZ=Asia/Kuala_Lumpur date '+%H' | sed 's/^0//')
-tokyo_hour=$(TZ=Asia/Tokyo date '+%H' | sed 's/^0//')
 sydney_hour=$(TZ=Australia/Sydney date '+%H' | sed 's/^0//')
 
 # Timeline header
@@ -28,7 +27,6 @@ draw_city "atlanta" "$atlanta_hour" "America/New_York"
 draw_city "oslo" "$oslo_hour" "Europe/Oslo"
 draw_city "jakarta" "$jakarta_hour" "Asia/Jakarta"
 draw_city "kuala lumpur" "$kuala_lumpur_hour" "Asia/Kuala_Lumpur"
-draw_city "tokyo" "$tokyo_hour" "Asia/Tokyo"
 draw_city "sydney" "$sydney_hour" "Australia/Sydney"
 
 echo ""
