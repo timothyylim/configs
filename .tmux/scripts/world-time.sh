e2ef4e4d942b49641a6b3200050554faa62c43da
timothyylim <timothylim23@gmail.com>
2026-02-23 15:21:11 +0800

    Add Atlanta back and show cities with times

--- .tmux/scripts/world-time.sh
+++ .tmux/scripts/world-time.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo ""
-echo "  $(TZ=Asia/Jakarta date '+%H:%M')"
-echo "  $(TZ=Asia/Kuala_Lumpur date '+%H:%M')"
-echo "  $(TZ=Asia/Tokyo date '+%H:%M')"
-echo "  $(TZ=Australia/Sydney date '+%H:%M')"
+echo "  atlanta      $(TZ=America/New_York date '+%H:%M')"
+echo "  jakarta      $(TZ=Asia/Jakarta date '+%H:%M')"
+echo "  kuala lumpur $(TZ=Asia/Kuala_Lumpur date '+%H:%M')"
+echo "  tokyo        $(TZ=Asia/Tokyo date '+%H:%M')"
+echo "  sydney       $(TZ=Australia/Sydney date '+%H:%M')"
 echo ""
