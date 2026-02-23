b2041f68a4bc54af78dc5105d6452834d9cf4551
timothyylim <timothylim23@gmail.com>
2026-02-23 15:20:37 +0800

    Remove US and Norway from world time zones

--- .tmux/scripts/world-time.sh
+++ .tmux/scripts/world-time.sh
@@ -4,8 +4,6 @@
 echo ""
 echo "  🌍 world time"
 echo ""
-echo "  atlanta    🇺🇸  $(TZ=America/New_York date '+%I:%M %p %Z')"
-echo "  oslo       🇳🇴  $(TZ=Europe/Oslo date '+%H:%M %Z')"
 echo "  jakarta    🇮🇩  $(TZ=Asia/Jakarta date '+%H:%M %Z')"
 echo "  kuala lumpur 🇲🇾  $(TZ=Asia/Kuala_Lumpur date '+%H:%M %Z')"
 echo "  tokyo      🇯🇵  $(TZ=Asia/Tokyo date '+%H:%M %Z')"
